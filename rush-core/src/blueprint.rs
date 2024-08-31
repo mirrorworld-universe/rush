a36c903c500b60c4f18e8db4ca6722dca1d8950e
Kristian Quirapas <kmquirapas@gmail.com>
2024-08-31 22:11:19 +0800

    feat: Core data types

--- rush-core/src/blueprint.rs
+++ rush-core/src/blueprint.rs
@@ -1,2 +1,50 @@
+use std::collections::BTreeMap;
+
+pub type Region = String;
+pub type Entity = String;
+pub type Component = String;
+pub type BlueprintString = String;
+
+/// Blueprint of a World
+///
+/// Represents a World in programmable data
 #[derive(Clone, Debug, Default)]
-pub struct Blueprint {}
+pub struct Blueprint {
+    /// World's Name
+    pub name: String,
+    /// Entity name and Names of its Comoponents
+    pub entities: BTreeMap<String, Vec<Component>>,
+    /// Names of available Regions
+    pub regions: Vec<String>,
+    /// Container of all instances existing in the World
+    pub instances: BTreeMap<Region, BTreeMap<Entity, BTreeMap<Component, ComponentValue>>>,
+}
+
+impl Blueprint {
+    pub fn new(world_name: String) -> Self {
+        Self {
+            name: world_name,
+            entities: BTreeMap::new(),
+            regions: Vec::new(),
+            instances: BTreeMap::new(),
+        }
+    }
+
+    pub fn add_entity(&mut self, name: Entity, components: Vec<Component>) {
+        self.entities.insert(name, components);
+    }
+
+    pub fn add_region(&mut self, name: Region) {
+        self.regions.push(name);
+    }
+}
+
+/// Enum defining the supported dataset in the World
+/// and how it maps with Rust data types
+#[derive(Clone, Debug)]
+pub enum ComponentValue {
+    String(String),
+    Integer(i64),
+    Float(f64),
+    Bool(bool),
+}
--- rush-core/src/lib.rs
+++ rush-core/src/lib.rs
@@ -1,2 +1,1 @@
 pub mod blueprint;
-pub mod utils;
