2a13a407f1d7eb764cebb4dd772b3c66f1220227
sceiiya <meetscheidj@gmail.com>
2024-12-15 01:05:36 +0800

    feat: instantiate sdk structure

---
+++ ecs/web/sdk/typescript/src/index.ts
@@ -0,0 +1,8 @@
+const set : string = 'bum'
+
+console.log(set)
+
+
+let momo : number = 12
+
+console.log(momo + 1)
---
+++ ecs/web/sdk/typescript/src/modules/storage/index.ts
@@ -0,0 +1,21 @@
+// ! in this solana
+
+
+// ? use solana client : rpc client, rpc client  
+// ! 
+
+// ! solana sdk :
+// ? pubkey,
+// ? signer: { keypair, keypair, signer }
+// ? transaction 
+
+// ! impl solana {
+// !  pub fn new(progam id: pubkey, signer: keypair, rpc_url: string, path: )
+// ! }
+
+
+class Solana {
+    constructor() {
+        
+    }
+}
