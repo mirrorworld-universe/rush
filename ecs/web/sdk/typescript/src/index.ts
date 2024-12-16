a99286d022da6f20439f4aeb2b3aaeab20a558e4
Skygild <skylevroan@gmail.com>
2024-12-16 22:38:01 +0800

    refactor: changed keypair passing to secret key

--- ecs/web/sdk/typescript/src/index.ts
+++ ecs/web/sdk/typescript/src/index.ts
@@ -1,28 +1,43 @@
 import { Keypair, PublicKey } from "@solana/web3.js";
 import { Solana } from "./modules/storage";
+import { RushSDK } from "./modules/sdk/sdk";
+import bs58 from "bs58";
 
 // ? this is a mock test
 // ? this is where the solana storage class is called inside the TS_RUSH_SDK
 
 function test_call_storage() {
-  const new_keypair = Keypair.generate();
+	const new_keypair = Keypair.generate();
 
-  const storage = new Solana({
-    blueprint: "/my/blueprint/path",
-    program_id: new PublicKey("6vg3oUN7LLcCS3Qc8bhsrqqJRkeDaC2KsFqF23aQp5iQ"),
-    signer: new_keypair,
-    rpc_url: "http://127.0.0.1:8899",
-  });
+	const storage = new Solana({
+		blueprint: "/my/blueprint/path",
+		program_id: new PublicKey("6vg3oUN7LLcCS3Qc8bhsrqqJRkeDaC2KsFqF23aQp5iQ"),
+		signer: new_keypair,
+		rpc_url: "http://127.0.0.1:8899",
+	});
 
-  console.log(storage);
-  console.log("signer :", {
-    "pubkey :": storage.signer.publicKey,
-    "sec key :": storage.signer.secretKey,
-  });
-  console.log(storage.signer.secretKey);
+	console.log(storage);
+	console.log("signer :", {
+		"pubkey :": storage.signer.publicKey,
+		"sec key :": storage.signer.secretKey,
+	});
+	console.log(storage.signer.secretKey);
+}
+
+function test_call_rushsdk() {
+	const new_keypair = Keypair.generate();
+	const encoded = bs58.encode(new_keypair.secretKey);
+	const secretKey = bs58.decode(encoded); // Pretend to be a secret key to be passed to creating the keypair
+	const sdk = new RushSDK({
+		secret_key: secretKey,
+		blueprint_path: "/my/blueprint/path",
+		program_id: new PublicKey("6vg3oUN7LLcCS3Qc8bhsrqqJRkeDaC2KsFqF23aQp5iQ"),
+		rpc_url: "http://127.0.0.1:8899",
+	});
+
+	console.log(sdk);
 }
 
 // test_call_storage();
-
-
-console.log('sdk index file')
+// test_call_rushsdk();
+console.log("sdk index file");
