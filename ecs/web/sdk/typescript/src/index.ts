0f06415694251871d7ea60a6d60a6165e58f8b98
sceiiya <meetscheidj@gmail.com>
2024-12-16 11:44:17 +0800

    feat: initialize solana storage class && test in the main src file

--- ecs/web/sdk/typescript/src/index.ts
+++ ecs/web/sdk/typescript/src/index.ts
@@ -1,8 +1,28 @@
-const set : string = 'bum'
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { Solana } from "./modules/storage";
 
-console.log(set)
+// ? this is a mock test
+// ? this is where the solana storage class is called inside the TS_RUSH_SDK
+
+function test_call_storage() {
+  const new_keypair = Keypair.generate();
+
+  const storage = new Solana({
+    blueprint: "/my/blueprint/path",
+    program_id: new PublicKey("6vg3oUN7LLcCS3Qc8bhsrqqJRkeDaC2KsFqF23aQp5iQ"),
+    signer: new_keypair,
+    rpc_url: "http://127.0.0.1:8899",
+  });
+
+  console.log(storage);
+  console.log("signer :", {
+    "pubkey :": storage.signer.publicKey,
+    "sec key :": storage.signer.secretKey,
+  });
+  console.log(storage.signer.secretKey);
+}
+
+// test_call_storage();
 
 
-let momo : number = 12
-
-console.log(momo + 1)
+console.log('sdk index file')
