707176996b71a39ad69f9a5954337771990dfeb1
Rjay <rjaysolamo@gmail.com>
2024-12-18 17:54:30 +0800

    Add set function implementation

--- ecs/web/sdk/typescript/src/modules/sdk/sdk.ts
+++ ecs/web/sdk/typescript/src/modules/sdk/sdk.ts
@@ -4,16 +4,32 @@ import { Keypair, PublicKey } from "@solana/web3.js";
 import bs58 from "bs58";
 
 export class RushSDK {
-	// Imporant variables
+	// Important variables
 	private keypair: Keypair;
 	private storage: Solana;
 
 	constructor({ rpc_url, program_id, blueprint_path, secret_key }: TSSDKParams) {
-		// Initialzation of the instance
+		// Initialization of the instance
 		this.keypair = Keypair.fromSecretKey(secret_key); // Create a key pair here using their secret key
 
 		const programIdKey = new PublicKey(program_id);
 
 		this.storage = new Solana({ blueprint: blueprint_path, program_id: programIdKey, rpc_url, signer: this.keypair });
+	}
+
+	/**
+	 * Public set function to update entity data in the on-chain world.
+	 * @param entityId - The ID of the entity to update.
+	 * @param data - The data to update for the entity.
+	 */
+	public async set(entityId: string, data: any) {
+		try {
+			const signature = await this.storage.set(entityId, data);
+			console.log("Set function executed successfully. Signature:", signature);
+			return signature;
+		} catch (error) {
+			console.error("Error in set function:", error);
+			throw error;
+		}
 	}
 }
