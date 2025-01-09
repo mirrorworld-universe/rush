ed5b96c6e38b6f834dad4f95d06287f9b87e034c
Hurotamo <166716974+Hurotamo@users.noreply.github.com>
2025-01-09 12:23:24 +0800

    Update worldPDA.ts

--- ecs/web/sdk/typescript/src/core/world/worldPDA.ts
+++ ecs/web/sdk/typescript/src/core/world/worldPDA.ts
@@ -1,36 +1,39 @@
 import { PublicKey } from "@solana/web3.js";
-import { Entity, Region } from "../types/Types";
 
 export class World {
-	discriminator: Uint8Array;
-	name: string;
-	description: string;
-	entities: Entity[];
-	regions: Region[];
-	instances: Map<Region, Map<Entity, number>>;
-	isLaunched: boolean;
-	worldAuthority: PublicKey;
-	bump: number;
+	static TAG: string = "World";
 
-	constructor(
-		discriminator: Uint8Array,
+	constructor() {}
+
+	static findPda(
+		program_id: PublicKey,
 		name: string,
 		description: string,
-		entities: Entity[],
-		regions: Region[],
-		instances: Map<Region, Map<Entity, number>>,
-		isLaunched: boolean,
+	): { pda: PublicKey; bump: number } {
+		const seed = [
+			Buffer.from(this.TAG),
+			Buffer.from(name),
+			Buffer.from(description),
+		];
+		const [pda, bump] = PublicKey.findProgramAddressSync(seed, program_id);
+		return { pda, bump };
+	}
+
+	static createPda(
+		programId: PublicKey,
+		name: string,
+		description: string,
 		worldAuthority: PublicKey,
-		bump: number,
-	) {
-		this.discriminator = discriminator;
-		this.name = name;
-		this.description = description;
-		this.entities = entities;
-		this.regions = regions;
-		this.instances = instances;
-		this.isLaunched = isLaunched;
-		this.worldAuthority = worldAuthority;
-		this.bump = bump;
+		bumpSeed: number,
+	): PublicKey {
+		const seed = [
+			Buffer.from(this.TAG),
+			Buffer.from(name),
+			Buffer.from(description),
+			worldAuthority.toBuffer(),
+			Buffer.from([bumpSeed]),
+		];
+		const pda = PublicKey.createProgramAddressSync(seed, programId);
+		return pda;
 	}
 }
