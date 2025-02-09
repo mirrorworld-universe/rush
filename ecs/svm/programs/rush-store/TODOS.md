d7bb430a3c0f4d77276da8de6b80ba7ef13bcd16
Kristian Quirapas <kmquirapas@gmail.com>
2025-02-09 18:05:00 +0800

    chore: Cleanup

---
+++ ecs/svm/programs/rush-store/TODOS.md
@@ -0,0 +1,5 @@
+# ToDo
+
+# Common Vulnerabilities Checklist
+
+# Optimizations Checklist
--- hasteImpl.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  getHasteName: (filePath) => {
-    // Custom logic to generate unique haste names
-    // You can modify this logic based on your project's structure
-    return filePath.replace(/.*[\/\\]/, ''); // Returns only the file name
-  },
-};
